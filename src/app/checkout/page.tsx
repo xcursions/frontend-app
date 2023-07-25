736820b38c04420e0dd1f8942161246b06a916a7
Justin Ikechukwu Anyanwu <justiniyke1995@gmail.com>
2023-07-25 04:12:52 +0100

    fix(event checkout): completed the checkout design

--- src/app/checkout/page.tsx
+++ src/app/checkout/page.tsx
@@ -1,25 +1,35 @@
 "use client";
 
+import Image from "next/image";
 import { useRouter, useSearchParams } from "next/navigation";
 import React, { useEffect, useState } from "react";
-import { AiOutlineArrowLeft } from "react-icons/ai";
+import { AiOutlineArrowLeft, AiOutlineClockCircle } from "react-icons/ai";
+import { BsFillArrowRightCircleFill } from "react-icons/bs";
+import { HiOutlineTicket } from "react-icons/hi";
+import { TbCalendar } from "react-icons/tb";
 
 import Button from "@/components/lib/Button";
+import {
+  formatDatesRange,
+  formatWeeksRange,
+} from "@/components/lib/FormatWeekRange/FormatWeekRage";
 import FullPageLoader from "@/components/lib/FullPageLoader";
 import Heading from "@/components/lib/Heading";
 import Input from "@/components/lib/Input";
+import { SubtractDate } from "@/components/lib/SubtractDate/SubtractDate";
 import Text from "@/components/lib/Text/Text";
 import Footer from "@/components/public/Footer/Footer";
 import Navbar from "@/components/public/Navbar";
 import Subscription from "@/components/public/Subscription/Subscription";
 import { useAppSelector, useAuth } from "@/hooks";
+import { useGetAllOutingsQuery } from "@/services/public";
 
 import styles from "./page.module.scss";
 
 const Page = () => {
   const router = useRouter();
   const searchParams = useSearchParams();
-  // const search = searchParams?.get("outing");
+  const search = searchParams?.get("outing");
   // @ts-ignore
   const count = parseInt(searchParams?.get("count"), 10);
   const [inputValues, setInputValues] = useState(
@@ -27,6 +37,7 @@ const Page = () => {
   );
   const { user } = useAppSelector((state) => state.user);
   const { isAuthenticated } = useAuth(true);
+  const { data } = useGetAllOutingsQuery(`/${search}`);
 
   useEffect(() => {
     if (!user) {
@@ -38,7 +49,7 @@ const Page = () => {
     newInputValues[index] = value;
     setInputValues(newInputValues);
   };
-  console.log(inputValues);
+
   return (
     <>
       {!isAuthenticated ? (
@@ -50,15 +61,15 @@ const Page = () => {
             <div className={styles.wrapper}>
               <div className={styles.container}>
                 <div className={styles.card_container}>
-                  <div className="rounded-2xl bg-[#ffffff] shadow-md">
-                    <div className="ml-[31px] mt-[40px]">
+                  <div className="mx-auto max-w-[818px] rounded-2xl bg-[#ffffff] shadow-md md:mx-0">
+                    <div className="ml-[12px] mr-2 mt-[40px] md:ml-[31px]">
                       <p className={styles.back} onClick={router.back}>
                         <AiOutlineArrowLeft /> Back
                       </p>
                       <Heading type="h1" className="mt-2 text-[30px]">
                         Checkout
                       </Heading>
-                      <div className="max-w-[700px] rounded-2xl border p-3">
+                      <div className="mb-[32px] mt-[48px] max-w-[700px] rounded-2xl border p-3">
                         <Text className="text-[12px] text-[#475467] ">
                           Email Address
                         </Text>
@@ -69,12 +80,12 @@ const Page = () => {
                       <div className="my-5 mr-2 items-center justify-center">
                         <hr className="border-t-1 grow border-[#E4E7EC]" />
                       </div>
-                      <div>
+                      <div className="md:mt-[25px]">
                         <Text className="font-dmSansBold text-[18px]">
                           Going with you ({count && count - 1})
                         </Text>
                         {count && count > 1 && (
-                          <Text>
+                          <Text className="mb-5 mt-3 text-[14px] text-[#475467]">
                             Enter the email address of people going with you
                           </Text>
                         )}
@@ -100,7 +111,103 @@ const Page = () => {
                       </div>
                     </div>
                   </div>
-                  <div></div>
+                  <div className="flex flex-col gap-4">
+                    <div className="mx-auto max-w-[427px] rounded-2xl bg-[#ffffff] shadow-md md:mx-0">
+                      <div className="ml-[30px] mt-[40px]">
+                        <Text className="font-dmSansBold text-[16px]">
+                          Event Summary
+                        </Text>
+                        <div className="mt-[32px] flex gap-3">
+                          <Image
+                            src={data?.outingGallery[0]?.image}
+                            alt={data?.name}
+                            width={100}
+                            height={100}
+                          />
+                          <div>
+                            <Text className="font-dmSansBold text-[18px] text-[#101828]">
+                              {data?.name}
+                            </Text>
+                            <Text className="font-dmSansBold text-[16px] text-[#0A83FF] ">
+                              ₦{parseInt(data?.price, 10).toLocaleString()}
+                            </Text>
+                          </div>
+                        </div>
+                        <Text className="mt-5 flex items-center gap-3 text-[14px]">
+                          <span className="font-dmSansMedium text-[16px] text-[#0A83FF]">
+                            <HiOutlineTicket />
+                          </span>
+                          {count} Ticket{count === 1 ? "" : "s"}
+                        </Text>
+                        <Text className=" my-5 flex gap-3 font-dmSansRegular text-[14px] text-[#475467]">
+                          <TbCalendar className=" text-xl text-[#475467]" />
+                          {formatDatesRange(
+                            data?.outingDate[0]?.startDate,
+                            data?.outingDate[0]?.endDate
+                          )}
+                        </Text>
+                        <Text className="flex items-center gap-3">
+                          <AiOutlineClockCircle />{" "}
+                          {Math.floor(
+                            formatWeeksRange(
+                              data?.outingDate[0]?.startDate,
+                              data?.outingDate[0]?.endDate
+                            )
+                          )}{" "}
+                          weeks
+                        </Text>
+                        <div className="my-7 mr-3 flex items-center gap-3 rounded-3xl bg-[#FFECEB] py-2">
+                          <AiOutlineClockCircle className="ml-3 text-[#F04438]" />{" "}
+                          <Text className="text-[12px] text-[#601B16]">
+                            Deadline for payment is{" "}
+                            {SubtractDate(
+                              data?.outingDate[0]?.startDate,
+                              data?.deadlineGap
+                            )}
+                          </Text>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="mx-auto max-w-[427px] rounded-2xl bg-[#ffffff] shadow-md md:mx-0">
+                      <div className="ml-[30px] mt-[40px]">
+                        <Text className="font-dmSansBold text-[16px]">
+                          Payment
+                        </Text>
+                        <div className="pr-3">
+                          <Input
+                            placeholder="Discount code here"
+                            endIcon={
+                              <BsFillArrowRightCircleFill className="text-[#0A83FF]" />
+                            }
+                          />
+                        </div>
+                        <div className="flex justify-between py-2 pr-3 text-[14px] text-[#667084]">
+                          <Text className="text-[14px]">No of tickets</Text>
+                          <span>{count}</span>
+                        </div>
+                        <div className="flex justify-between py-2 pr-3 text-[14px] text-[#667084]">
+                          <Text className="text-[14px]">Discount</Text>
+                          <span>₦0</span>
+                        </div>
+                        <div className="my-5 mr-2 items-center justify-center">
+                          <hr className="border-t-1 grow border-[#E4E7EC]" />
+                        </div>
+                        <div className="flex justify-between pb-5 pr-3">
+                          <Text className="text-[14px] text-[#667084]">
+                            Total
+                          </Text>
+                          <span className="text-[18px] text-[#101828] ">
+                            ₦
+                            {data?.price
+                              ? (
+                                  parseInt(data.price, 10) * count
+                                ).toLocaleString()
+                              : "Not available"}
+                          </span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
---
+++ src/components/lib/FormatWeekRange/FormatWeekRage.ts
@@ -0,0 +1,41 @@
+const formatDate = (dateString: string | number | Date) => {
+  const date = new Date(dateString);
+  const day = date.getDate();
+  const month = date.toLocaleString("default", { month: "short" });
+
+  let daySuffix = "th";
+  if (day === 1 || day === 21 || day === 31) {
+    daySuffix = "st";
+  } else if (day === 2 || day === 22) {
+    daySuffix = "nd";
+  } else if (day === 3 || day === 23) {
+    daySuffix = "rd";
+  }
+
+  return `${day}${daySuffix} ${month}`;
+};
+
+export const formatDatesRange = (
+  startDateString: string | number | Date,
+  endDateString: string | number | Date
+) => {
+  const startDate = formatDate(startDateString);
+  const endDate = formatDate(endDateString);
+
+  return `${startDate} - ${endDate}`;
+};
+
+export const formatWeeksRange = (
+  startDateString: string | number | Date,
+  endDateString: string | number | Date
+) => {
+  const startDate = new Date(startDateString);
+  const endDate = new Date(endDateString);
+  // @ts-ignore
+  const timeDifference = endDate - startDate;
+
+  // Calculate the number of weeks in the duration
+  const millisecondsInAWeek = 7 * 24 * 60 * 60 * 1000;
+  const numberOfWeeks = timeDifference / millisecondsInAWeek;
+  return numberOfWeeks;
+};
