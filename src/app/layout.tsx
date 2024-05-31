e3a5fd4198d6e7b488fecd1a18d353ce9b343c0e
Justin Ikechukwu Anyanwu <justiniyke1995@gmail.com>
2024-05-31 12:09:07 +0100

    feat(facebook pixel): added facebook pixel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -108,22 +108,50 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <Script
-        async
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GTAG}`}
-      />
-      <Script
-        id="g_tag"
-        dangerouslySetInnerHTML={{
-          __html: `  window.dataLayer = window.dataLayer || [];
+      <head>
+        <Script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GTAG}`}
+        />
+        <Script
+          id="g_tag"
+          dangerouslySetInnerHTML={{
+            __html: `  window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
         gtag('js', new Date());
 
         gtag('config','${process.env.NEXT_PUBLIC_GTAG}', {
           page_path: window.location.pathname,
         })`,
-        }}
-      />
+          }}
+        />
+        <Script
+          id="facebook-pixel"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '801857154803217');
+            fbq('track', 'PageView');
+          `,
+          }}
+        />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=801857154803217&ev=PageView&noscript=1"
+          />
+        </noscript>
+      </head>
       <body
         className={cn(
           `${dmSans.className} relative min-h-screen bg-background antialiased`,
