acacb3002b2e1d7f582b8d0b7b309504c9b73f65
Ikechukwu Anyanwu <justiniyke1995@gmail.com>
2025-05-28 05:14:54 +0100

    feat(visa): added ability to pay for visa booking using paystack

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -137,8 +137,23 @@ export default function RootLayout({
             t.src=v;s=b.getElementsByTagName(e)[0];
             s.parentNode.insertBefore(t,s)}(window, document,'script',
             'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '801857154803217');
+            fbq('init', '${
+              process.env.NEXT_PUBLIC_FACEBOOK_PIXEL_ID ?? 801857154803217
+            }');
             fbq('track', 'PageView');
+          `,
+        }}
+      />
+      <Script
+        id="twitter-pixel"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+          !function(e,t,n,s,u,a){e.twq||(s=e.twq=function(){s.exe?s.exe.apply(s,arguments):s.queue.push(arguments);
+},s.version='1.1',s.queue=[],u=t.createElement(n),u.async=!0,u.src='https://static.ads-twitter.com/uwt.js',
+a=t.getElementsByTagName(n)[0],a.parentNode.insertBefore(u,a))}(window,document,'script');
+twq('config','${process.env.NEXT_PUBLIC_X_TAG}');
+
           `,
         }}
       />
